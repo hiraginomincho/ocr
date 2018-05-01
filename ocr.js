d0a8e7d0f16b3af9f49de2c4750623c9e5184db6
HM <hiraginomincho@users.noreply.github.com>
2018-05-01 02:57:12 -0400

    more improvements

--- ocr.js
+++ ocr.js
@@ -2,14 +2,29 @@
 
 console.log("OCR: Using Tesseract.js version " + Tesseract.version);
 
+class Word {
+  constructor(word, x0, y0, x1, y1, confidence, endOfWord) {
+    this.word = word;
+    this.x0 = x0;
+    this.y0 = y0;
+    this.x1 = x1;
+    this.y1 = y1;
+    this.confidence = confidence;
+    this.endOfWord = endOfWord;
+  }
+}
+
+var wordList = [];
+var text = "";
+var filteredWordList = [];
+var filteredText = "";
+
 var res;
 
 var scale;
 
-var showBoundingBoxes = true;
 var boundingBoxColor = "#00ff00";
 
-var showText = true;
 var fontSize = 24;
 var textColor = "#0000ff";
 
@@ -42,38 +57,94 @@ function clear() {
   ctx.clearRect(0, 0, overlay.width, overlay.height);
 }
 
+function filterText() {
+  filteredWordList = [];
+  for (let i = 0; i < wordList.length; i++) {
+    var word = new Word(wordList[i].word, wordList[i].x0, wordList[i].y0, wordList[i].x1, wordList[i].y1, wordList[i].confidence, wordList[i].endOfWord);
+    filteredWordList.push(word);
+  }
+  var i = 0;
+  while (i < filteredWordList.length) {
+    if (filteredWordList[i].confidence < confidenceThreshold) {
+      if (i > 0) {
+        filteredWordList[i - 1].endOfWord = filteredWordList[i].endOfWord;
+      }
+      filteredWordList.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+  filteredText = "";
+  for (let i = 0; i < filteredWordList.length; i++) {
+    filteredText += filteredWordList[i].word + filteredWordList[i].endOfWord;
+  }
+}
+
+function drawBoundingBox(i, label) {
+  ctx.lineWidth = 4;
+  ctx.strokeStyle = boundingBoxColor;
+  ctx.strokeRect(wordList[i].x0 * scale, wordList[i].y0 * scale, (wordList[i].x1 - wordList[i].x0) * scale, (wordList[i].y1 - wordList[i].y0) * scale);
+  if (label !== "") {
+    ctx.font = fontSize + "px sans-serif";
+    // ctx.textBaseline = "hanging";
+    var displayText = label; // wordList[i].word + " - " + wordList[i].confidence.toFixed(1);
+    var width = ctx.measureText(displayText).width;
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(wordList[i].x0 * scale, wordList[i].y0 * scale - fontSize, width, fontSize);
+    ctx.fillStyle = textColor;
+    ctx.fillText(displayText, wordList[i].x0 * scale, wordList[i].y0 * scale);
+  }
+}
+
+function setConfidence(confidence) {
+  confidenceThreshold = confidence;
+  filterText();
+  OCR.gotFilteredText(filteredText);
+  // drawBoundingBoxes();
+}
+
+function listWords() {
+  var wList = [];
+  for (let i = 0; i < wordList.length; i++) {
+    var wordAndConfidence = [];
+    wordAndConfidence.push(wordList[i].word);
+    wordAndConfidence.push(wordList[i].confidence);
+    wList.push(wordAndConfidence);
+  }
+  return wList;
+}
+
 function recognize(image) {
+  wordList = [];
   Tesseract.recognize(image, options)
   .progress(function (p) {
     console.log("progress", p);
-    OCR.reportProgress(p.status);
+    OCR.gotProgress(p.status);
   })
   .then(function (result) {
-    var textString = "";
-    if (showBoundingBoxes) {
-      result.words.forEach(function(w) {
-        if (w.confidence > confidenceThreshold) {
-          var b = w.bbox;
-          ctx.lineWidth = 4;
-          ctx.strokeStyle = boundingBoxColor;
-          ctx.strokeRect(b.x0 * scale, b.y0 * scale, (b.x1 - b.x0) * scale, (b.y1 - b.y0) * scale);
-          if (showText) {
-            ctx.font = fontSize + "px sans-serif";
-            // ctx.textBaseline = "hanging";
-            var width = ctx.measureText(w.text).width;
-            ctx.fillStyle = "#ffffff";
-            ctx.fillRect(b.x0 * scale, b.y0 * scale - fontSize, width, fontSize);
-            ctx.fillStyle = textColor;
-            ctx.fillText(w.text, b.x0 * scale, b.y0 * scale);
-          }
-          textString += w.text + " ";
+    text = result.text;
+    var index = 0;
+    result.words.forEach(function(w) {
+      var wordLoc = result.text.indexOf(w.text, index);
+      if (wordLoc === -1) {
+        console.log("ERROR");
+      } else {
+        var word = new Word(w.text, w.bbox.x0, w.bbox.y0, w.bbox.x1, w.bbox.y1, w.confidence, "");
+        wordList.push(word);
+        if (wordList.length > 1) {
+          wordList[wordList.length - 2].endOfWord = result.text.substring(index, wordLoc);
+          index += result.text.substring(index, wordLoc).length;
         }
-      });
-    }
+        index += w.text.length;
+      }
+    });
+    wordList[wordList.length - 1].endOfWord = result.text.substring(index);
+    filterText();
     res = result;
-    console.log("result", result);
-    // OCR.reportResult(result.text);
-    OCR.reportResult(textString);
+    console.log("OCR: result is " + text);
+    OCR.gotText(text);
+    OCR.gotFilteredText(filteredText);
+    OCR.gotWords(JSON.stringify(listWords()));
   });
 }
 
@@ -102,6 +173,9 @@ document.body.appendChild(video);
 
 video.addEventListener("loadedmetadata", function() {
   video.height = this.videoHeight * video.width / this.videoWidth;
+  overlay.width = video.width;
+  overlay.height = video.height;
+  scale = video.width / video.videoWidth;
 }, false);
 
 function startVideo() {
@@ -141,9 +215,6 @@ function recognizeImageData(imageData) {
 
 function recognizeVideoData() {
   if (isVideoMode) {
-    overlay.width = video.width;
-    overlay.height = video.height;
-    scale = video.width / video.videoWidth;
     recognize(video);
   }
 }
@@ -167,6 +238,9 @@ window.addEventListener("resize", function() {
   img.width = window.innerWidth;
   video.width = window.innerWidth;
   video.height = video.videoHeight * window.innerWidth / video.videoWidth;
+  overlay.width = video.width;
+  overlay.height = video.height;
+  scale = video.width / video.videoWidth;
 });
 
 OCR.ready();
